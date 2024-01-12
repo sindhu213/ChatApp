1192690495d9cee3b9d30dd78aa2e405767a10d2
Sindhu <ksindhu571@gmail.com>
2024-01-12 21:00:14 +0530

    Basic authentication - done

---
+++ apps/web/app/lib/utils.ts
@@ -0,0 +1,22 @@
+import prisma from "app//lib/prismadb";
+
+export async function getUser(userEmail ?: string) {
+    if(userEmail === undefined){
+        console.error("User-email is undefined.");
+        return null;
+    }
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: userEmail,
+            },
+        });
+        return user;
+    } catch (error) {
+        console.log("Failed to fetch data: ", error);
+        throw new Error("Failed to fetch data.");
+    } finally {
+        await prisma.$disconnect();
+    }
+}
